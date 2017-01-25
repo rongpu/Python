53c7f3740968d96b8b677fc7f64c74e55d911337
Rongpu Zhou <rongpu.zhou@gmail.com>
2017-01-25 15:52:17 -0500

    Minor changes

--- solid_angle.py
+++ solid_angle.py
@@ -1,15 +1,15 @@
 from __future__ import division, print_function
 import numpy as np
 
-def solid_angle(phi1, theta1, phi2, theta2, return_steradian=True):
+def solid_angle(ra1, dec1, ra2, dec2, return_steradian=True):
     '''
     Calculate the solid angle of a rectangle in the sky. The rectangle 
-    is defined by its two corners (phi1, theta1) and (phi2, theta2), 
+    is defined by its two corners (ra1, dec1) and (ra2, dec2), 
     where theta is the polar angle, in units of degrees. 
 
     Returns solid angle in steradian by default. 
     '''
-    omega = np.abs(np.pi/180.*(phi2 - phi1)*(np.sin(theta2*np.pi/180) - np.sin(theta1*np.pi/180)))
+    omega = np.abs(np.pi/180.*(ra2 - ra1)*(np.sin(dec2*np.pi/180) - np.sin(dec1*np.pi/180)))
     if return_steradian:
         return omega
     else:
