eaaf195ce0c842711a873a3c9747bae155a6154b
Rongpu Zhou <rongpu.zhou@gmail.com>
2019-06-05 13:27:41 -0400

    add an improved h-l mean estimator

--- user_modules/user_common.py
+++ user_modules/user_common.py
@@ -5,44 +5,7 @@ from scipy.interpolate import interp1d
 
 def hlmean(data, multiplier=None, verbose=True):
     '''
-    PURPOSE:
-
-     Calculate Hodges-Lehmann estimator of mean, using nsamp
-      bootstraps from the data
-
-    The Hodges-Lehmann estimator is, formally, the median value of
-     (x_i+x_j)/2 over all pairs of indices i,j .
-
-    If there are less than 200 samples, we compute the formal Hodge-
-    Lehmann mean. Otherwise, we estimate that quantity using nsamp 
-    randomly chosen values of i & j, rather than using all possible 
-    values.
-
-    Although it has much of the robustness of an ordinary median, the
-    H-L estimator yields much smaller errors (equivalent to the mean of
-    >90% as much data, while the median has errors equivalent to the
-    standard error of 64% as much data).
-    CALLING SEQUENCE:
-
-     result=hlmean(data [,nsamp=nsamp])
-
-    INPUTS:
-
-     data: array of values to calculate the H-L mean of
-
-    OPTIONAL KEYWORD PARAMETERS:
-
-     multiplier : if set, hlmean will use this number of bootstrap
-     samples to do the calculation.  If not set, it will use 200*the
-     number of elements of the data array random values of i&j.
-
-    OUTPUTS:
-
-     result: bootstrap estimate of the H-L mean estimator
-
-    EXAMPLE:
-       test=[1,2,0,1,2,20.]
-       print hlmean(test)
+    Hodges-Lehmann estimator.
     '''
 
     ndata = len(data)
@@ -64,6 +27,35 @@ def hlmean(data, multiplier=None, verbose=True):
         idx = idx.astype(np.int64,copy=False)
         pairmean = np.sum(data[idx],axis=1)/2.
     return(np.median(pairmean))
+
+# ---------------------------------------------------
+
+def hlmean_v2(data, maxpairs=1e7, verbose=True):
+    '''
+    An improved version of the Hodges-Lehmann estimator.
+    '''
+
+    import itertools
+
+    maxpairs = int(maxpairs)
+    ndata = len(data)
+    
+    if ndata==0:
+        if verbose: print('H-L mean: empty array!!!')
+        return None
+    if ndata*(ndata-1)/2 <= maxpairs:
+        # only non-identical indices are included
+        idx1, idx2 = np.array(list(itertools.combinations(np.arange(ndata), 2))).transpose()
+        pairmean1 = (data[idx1]+data[idx2])/2.
+        # the pairwise mean of identical indices
+        pairmean2 = data
+        pairmean = np.concatenate([pairmean1, pairmean2])
+        hlmean = np.median(pairmean)
+    else:
+        idx1, idx2 = np.random.choice(ndata, size=(maxpairs, 2)).transpose()
+        pairmean = (data[idx1]+data[idx2])/2.
+        hlmean = np.median(pairmean)
+    return(hlmean)
 
 # ---------------------------------------------------
 
